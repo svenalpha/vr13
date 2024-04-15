30e0f284ba02b724cac5b6d60079c166b0ab4a73
svenalpha <svalphaforsale@yahoo.com>
2024-04-15 18:58:10 +0700

    message

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -40,7 +40,7 @@ const Home: React.FC<HomeProps> = (_props) => {
     return (
         <>
             <div>
-                <h4>version      14      19:06      15/04/2024   </h4>
+                <h4>version      15      18:57      15/04/2024   </h4>
                 <p>{legend}</p>
                 <p>{legend1}</p>
                 <a href="https://vitejs.dev" target="_blank">
