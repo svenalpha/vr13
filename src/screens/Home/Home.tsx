5f5ff0f076eab586ecfce10dfb4fe6783f9f391b
svenalpha <svalphaforsale@yahoo.com>
2024-04-16 18:50:44 +0700

    message

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -40,7 +40,7 @@ const Home: React.FC<HomeProps> = (_props) => {
     return (
         <>
             <div>
-                <h4>version      17      15:23     16/04/2024   </h4>
+                <h4>version      18      18:47     16/04/2024   </h4>
                 <p>{legend}</p>
                 <p>{legend1}</p>
                 <a href="https://vitejs.dev" target="_blank">
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({  
+  build:{
+         outDir: "./dist/app/client"
+        },
   server: {
     proxy: {
      // "/api/v1": "http://localhost:3000/",
