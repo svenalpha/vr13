e864c43be37f5d6799e65515dd81463aa6c36623
svenalpha <svalphaforsale@yahoo.com>
2024-04-24 19:23:24 +0700

    message

--- dist/entry/entry-server.js
+++ dist/entry/entry-server.js
@@ -899,7 +899,7 @@ const Home = (_props) => {
   }, []);
   return /* @__PURE__ */ jsxs(Fragment, { children: [
     /* @__PURE__ */ jsxs("div", { children: [
-      /* @__PURE__ */ jsx("h4", { children: "version      25      14:40     24/04/2024   " }),
+      /* @__PURE__ */ jsx("h4", { children: "version      26      18:33     24/04/2024   " }),
       /* @__PURE__ */ jsx("p", { children: legend }),
       /* @__PURE__ */ jsx("p", { children: legend1 }),
       /* @__PURE__ */ jsx("a", { href: "https://vitejs.dev", target: "_blank", children: /* @__PURE__ */ jsx("img", { src: viteLogo, className: styles.logo, alt: "Vite logo" }) }),
